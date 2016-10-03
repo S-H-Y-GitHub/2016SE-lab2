51d1eb25114839bf3767c9fab63dc242783a1a3e
S-H-Y-GitHub <932495000@qq.com>
2016-10-03 13:05:42 +0800

    加入配置文件
    Signed-off-by: S-H-Y-GitHub <932495000@qq.com>

--- src/book/dao/BookDao.java
+++ src/book/dao/BookDao.java
@@ -7,15 +7,16 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 public class BookDao {
+    // JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/bookdb";
+
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "root";
+    static final String PASS = "zzyadmin";
     public static void main(String[] args) throws Exception {
         Connection conn = null;
         String sql;
-        // MySQL的JDBC URL编写方式：jdbc:mysql://主机名称：连接端口/数据库的名称?参数=值
-        // 避免中文乱码要指定useUnicode和characterEncoding
-        // 执行数据库操作之前要在数据库管理系统上创建一个数据库，名字自己定，
-        String url = "jdbc:mysql://localhost:3306/bookdb?"
-                + "user=root&password=zzyadmin&useUnicode=true&characterEncoding=UTF8";
-
         try {
             // 之所以要使用下面这条语句，是因为要使用MySQL的驱动，所以我们要把它驱动起来，
             // 可以通过Class.forName把它加载进去，也可以通过初始化来驱动起来，下面三种形式都可以
@@ -27,7 +28,7 @@ public class BookDao {
 
             System.out.println("成功加载MySQL驱动程序");
             // 一个Connection代表一个数据库连接
-            conn = DriverManager.getConnection(url);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
             // Statement里面带有很多方法，比如executeUpdate可以实现插入，更新和删除等
             Statement stmt = conn.createStatement();
             sql = "select * from bookdb.book";
@@ -35,7 +36,7 @@ public class BookDao {
             System.out.println("学号\t姓名");
             while (rs.next()) {
                 System.out
-                        .println(rs.getString(1) + "\t" + rs.getString(2));// 入如果返回的是int类型可以用getInt()
+                        .println(rs.getString("ISBN") + "\t" + rs.getString("Title"));// 如果返回的是int类型可以用getInt()
             }
         } catch (SQLException e) {
             System.out.println("MySQL操作错误");
@@ -43,7 +44,9 @@ public class BookDao {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            conn.close();
+            if (conn != null) {
+                conn.close();
+            }
         }
 
     }
